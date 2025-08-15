161b861eefb0996ceefdeea1a09b2630968e1012
viodid <david.yunta.aller@gmail.com>
2025-08-15 19:09:00 +0200

    1_3

---
+++ go/ch1/1_3.go
@@ -0,0 +1,32 @@
+package main
+
+// constraints: time complexity should be at most O(n)
+
+// traverse the input slice backwards with two pointers:
+// frist starting from the tLength idx without the right-hand buffer offset
+// as the read cursor
+// second from the end of the slice as the write cursor
+
+// time: O(n) - space: O(1)
+func URLify(inputStr []byte, tLength int) {
+	if inputStr == nil || len(inputStr) == 0 {
+		return
+	}
+	writeCur := len(inputStr) - 1
+	
+	for readCur := tLength - 1; readCur >= 0; readCur-- {
+		if inputStr[readCur] == ' ' {
+			inputStr[writeCur] = '0'
+			writeCur--
+			inputStr[writeCur] = '2'
+			writeCur--
+			inputStr[writeCur] = '%'
+			writeCur--
+		} else {
+			inputStr[writeCur] = inputStr[readCur]
+			writeCur--
+		}
+	}
+
+}
+
---
+++ go/ch1/1_3_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestURLify(t *testing.T) {
+	tests := []struct{
+		inputStr string
+		length int
+		expectedStr string
+	}{
+		{ "Mr John Smith    ", 13, "Mr%20John%20Smith" },
+		{ "Mr John  Smith      ", 14, "Mr%20John%20%20Smith" },
+		{ " Mr John Smith      ", 14, "%20Mr%20John%20Smith" },
+		{ "   ", 1, "%20" },
+		{ "", 0, "" },
+		{ "hey    there        ", 12, "hey%20%20%20%20there" },
+		{ "hey there  ", 9, "hey%20there" },
+		{ "hey", 3, "hey" },
+	}
+
+	for _, tt := range tests {
+		prevStr := []byte(tt.inputStr)
+		URLify(prevStr, tt.length)
+		if string(prevStr) != tt.expectedStr {
+			t.Errorf("URLify(%s, %d) hasn't mutate the string as '%s'\n",
+			prevStr, tt.length, tt.expectedStr)
+		}
+	}
+}
