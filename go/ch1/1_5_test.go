e1081747198ebe23a50584b48e33c357cb252eac
viodid <david.yunta.aller@gmail.com>
2025-08-17 12:53:12 +0200

    1_5 utest

---
+++ go/ch1/1_5_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestOneWay(t *testing.T) {
+	tests := []struct{
+		s1 string
+		s2 string
+		expected bool
+	}{
+		{ "pale", "ple", true },
+		{ "pales", "pale", true },
+		{ "pale", "bale", true },
+		{ "pale", "bake", false },
+		{ "", "", true },
+		{ "pale", "pale", true },
+		{ "a", "", true },
+		{ "ab", "", false },
+	}
+
+	for _, tt := range tests {
+		if oneWay(tt.s1, tt.s2) != tt.expected {
+			t.Errorf("oneWay(\"%s\", \"%s\") is not %t. got=%t",
+				tt.s1, tt.s2, tt.expected, oneWay(tt.s1, tt.s2))
+		}
+	}
+
+}
