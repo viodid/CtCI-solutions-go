cc3e4f1f48e954fe10ae3c95051bd7b1cfc2a300
viodid <david.yunta.aller@gmail.com>
2025-09-01 20:52:21 +0200

    2_1 implementation with buffer state storage

--- go/ch2/2_1.go
+++ go/ch2/2_1.go
@@ -1,5 +1,25 @@
 package main
 
-func removeDups(input *LinkedList[int]) *LinkedList[int] {
-	return input
+// iterate over the LL and store the repetition state.
+// If an int is found more than once, remove the node
+// time: O(n) - space: O(n)
+func removeDups(ll *LinkedList[int]) *LinkedList[int] {
+	if ll == nil || ll.head == ll.tail {
+		return ll
+	}
+	n1 := ll.head
+	n2 := ll.head.next
+	repetitionState := map[int]bool{n1.content: true}
+	for n2 != nil {
+		_, ok := repetitionState[n2.content]
+		if ok {
+			n1.next = n2.next
+		} else {
+			repetitionState[n2.content] = true
+		}
+		n1 = n1.next
+		n2 = n2.next
+	}
+	return ll
 }
+
