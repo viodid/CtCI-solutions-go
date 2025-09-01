f70eccae107c5d2172e39a865d911420b7f8791c
viodid <david.yunta.aller@gmail.com>
2025-09-01 18:50:56 +0200

    1_9 test and implementation

---
+++ go/ch1/1_9.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+// time: O(n) - space: O(1)
+func stringRotation(s1, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	return strings.Contains(s1 + s1, s2)
+}
---
+++ go/ch1/1_9_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestStringRotation(t *testing.T) {
+	tests := []struct{
+		input []string
+		expected bool
+	}{
+		{
+			input: []string{"", ""},
+			expected: true,
+		},
+		{
+			input: []string{"a", "a"},
+			expected: true,
+		},
+		{
+			input: []string{"a", "ab"},
+			expected: false,
+		},
+		{
+			input: []string{"erbottlewat", "waterbottle"},
+			expected: true,
+		},
+		{
+			input: []string{"erottlewat", "waterbottle"},
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		output := stringRotation(tt.input[0], tt.input[1])
+		if output != tt.expected {
+			t.Errorf("stringRotation(\"%s\", \"%s\") is not %t. got=%t\n",
+				tt.input[0], tt.input[1], output, tt.expected)
+		}
+	}
+}
