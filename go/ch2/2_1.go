fc3ec66b619ef02ecd6d57ce680207c2f22761e2
viodid <david.yunta.aller@gmail.com>
2025-09-02 13:07:53 +0200

    2_1 v2 implementation

--- go/ch2/2_1.go
+++ go/ch2/2_1.go
@@ -25,9 +25,27 @@ func removeDups(ll *LinkedList[int]) *LinkedList[int] {
 	return ll
 }
 
-// for this version the constraint is that no buffer is permitted to store state
-// so when the LL is sorted, is enough to keep just adjacent numbers to delete duplicates
-// time: O(nlogn) - space: O(1)
-// func removeDupsv2(ll *LinkedList[int]) *LinkedList[int] {
-// 	// TODO
-// }
+// For this version the constraint is that no buffer is permitted to store state.
+// So n^2 comparatinos are performed to remove duplicates
+// time: O(n^2) - space: O(1)
+func removeDupsv2(ll *LinkedList[int]) *LinkedList[int] {
+	if ll == nil || ll.head.next == nil {
+		return ll
+	}
+	curr := ll.head
+	for curr != nil && curr.next != nil {
+		prevComp := curr
+		comp := curr.next
+		for comp != nil {
+			if curr.content == comp.content {
+				prevComp.next = comp.next
+				comp = comp.next
+				continue
+			}
+			prevComp = prevComp.next
+			comp = comp.next
+		}
+		curr = curr.next
+	}
+	return ll
+}
