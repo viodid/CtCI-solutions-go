d371fbdf8ea7ee2f74b7e10718f70d00037efcd6
viodid <david.yunta.aller@gmail.com>
2025-09-05 11:29:20 +0200

    kth last node v2 two pointers implementation

--- go/ch2/2_2.go
+++ go/ch2/2_2.go
@@ -5,21 +5,19 @@ import "ch2/ll"
 // Get the size of the linked list in one iteration
 // Return the corresponding index
 // time: O(n) - space: O(1)
-func returnKthToLast(ll *ll.LinkedList[int], idx int) *ll.Node[int] {
-	if ll == nil {
+func returnKthToLast(list *ll.LinkedList[int], k int) *ll.Node[int] {
+	if list == nil {
 		return nil
-	} else if ll.Head.Next == nil && idx == 1 {
-		return ll.Head
 	}
 
 	size := 0
-	node := ll.Head
+	node := list.Head
 	for node != nil {
 		size++
 		node = node.Next
 	}
-	node = ll.Head
-	nodeIdx := size - idx
+	node = list.Head
+	nodeIdx := size - k
 	cursor := 0
 	for node != nil {
 		if cursor == nodeIdx {
@@ -30,3 +28,35 @@ func returnKthToLast(ll *ll.LinkedList[int], idx int) *ll.Node[int] {
 	}
 	return nil
 }
+
+// Another way of solving this problem is by using two pointers: p1 and p2
+// p1 will advance kth nodes
+// iterate through the ll with p1 and p2 at the same pace
+// when p2 reaches the end, p1 will be at the right spot
+// this improves slightly the time complexity but it is still O(n)
+// time: O(n) - space: O(1)
+func returnKthToLastv2(list *ll.LinkedList[int], k int) *ll.Node[int] {
+	if list == nil {
+		return nil
+	}
+	
+	p1 := list.Head
+	p2 := list.Head
+	i := 1
+	for p2 != nil {
+		if i == k {
+			break
+		}
+		p2 = p2.Next
+		i++
+	}
+	if p2 == nil {
+		return nil
+	}
+	for p2.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	return p1
+}
+
--- go/ch2/2_2_test.go
+++ go/ch2/2_2_test.go
@@ -54,16 +54,85 @@ func TestReturnKthToLast(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		if tt.expected == nil {
-			continue
-		}
 		node := returnKthToLast(tt.list, tt.idx)
-		if tt.expected == nil && node != nil {
-			t.Errorf("returnKthToLast(nil, %d) does not return expected=%+v\n", tt.idx, tt.expected)
+		if tt.expected == nil {
+			if  node != nil {
+				t.Errorf("returnKthToLast(nil, %d) does not return expected=%+v\n", tt.idx, tt.expected)
+			}
 			continue
 		}
 		if node.Content != tt.expected.Content {
 			t.Errorf("returnKthToLast failed. got=%d expected=%d idx=%d\n",
+				node.Content, tt.expected.Content, tt.idx)
+			continue
+		}
+	}
+}
+
+func TestReturnKthToLastv2(t *testing.T) {
+	tests := []struct {
+		list *ll.LinkedList[int]
+		idx int
+		expected *ll.Node[int]
+	}{
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			1,
+			ll.NewNode(0),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			2,
+			ll.NewNode(1),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			3,
+			ll.NewNode(2),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			4,
+			ll.NewNode(3),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			0,
+			nil,
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			5,
+			nil,
+		},
+		{ 
+			nil,
+			5,
+			nil,
+		},
+		{ 
+			ll.CreateLinkedList([]int{3}),
+			1,
+			ll.NewNode(3),
+		},
+	}
+
+	for _, tt := range tests {
+		node := returnKthToLastv2(tt.list, tt.idx)
+		if tt.expected == nil  {
+			if node != nil {
+				t.Errorf("returnKthToLastv2(%+v, %d) does not return expected=%+v got=%+v\n",
+					tt.expected, tt.idx, tt.expected, node)
+			}
+			continue
+		}
+		if node == nil {
+			t.Errorf("returnKthToLastv2 failed. got=%+v expected=%d idx=%d\n",
+				node, tt.expected.Content, tt.idx)
+			continue
+		}
+		if node.Content != tt.expected.Content {
+			t.Errorf("returnKthToLastv2 failed. got=%d expected=%d idx=%d\n",
 				node.Content, tt.expected.Content, tt.idx)
 			continue
 		}
