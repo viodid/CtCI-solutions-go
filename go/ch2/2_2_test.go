b80e629a7d65b5af630f8f3a9ff592c07019ba19
viodid <david.yunta.aller@gmail.com>
2025-09-08 18:59:09 +0200

    2_2 v3 recursive tests

--- go/ch2/2_2_test.go
+++ go/ch2/2_2_test.go
@@ -138,3 +138,73 @@ func TestReturnKthToLastv2(t *testing.T) {
 		}
 	}
 }
+
+func TestReturnKthToLastv3(t *testing.T) {
+	tests := []struct {
+		list *ll.LinkedList[int]
+		idx int
+		expected *ll.Node[int]
+	}{
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			1,
+			ll.NewNode(0),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			2,
+			ll.NewNode(1),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			3,
+			ll.NewNode(2),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			4,
+			ll.NewNode(3),
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			0,
+			nil,
+		},
+		{ 
+			ll.CreateLinkedList([]int{3, 2, 1, 0}),
+			5,
+			nil,
+		},
+		{ 
+			nil,
+			5,
+			nil,
+		},
+		{ 
+			ll.CreateLinkedList([]int{3}),
+			1,
+			ll.NewNode(3),
+		},
+	}
+
+	for _, tt := range tests {
+		node := returnKthToLastv3(tt.list, tt.idx)
+		if tt.expected == nil  {
+			if node != nil {
+				t.Errorf("returnKthToLastv3(%+v, %d) does not return expected=%+v got=%+v\n",
+					tt.expected, tt.idx, tt.expected, node)
+			}
+			continue
+		}
+		if node == nil {
+			t.Errorf("returnKthToLastv3 failed. got=%+v expected=%d idx=%d\n",
+				node, tt.expected.Content, tt.idx)
+			continue
+		}
+		if node.Content != tt.expected.Content {
+			t.Errorf("returnKthToLastv3 failed. got=%d expected=%d idx=%d\n",
+				node.Content, tt.expected.Content, tt.idx)
+			continue
+		}
+	}
+}
