492240c474ca3801d1a250a904dd027e457557ac
viodid <david.yunta.aller@gmail.com>
2025-09-18 21:21:45 +0200

    2_4 test bugifxes

--- go/ch2/2_4_test.go
+++ go/ch2/2_4_test.go
@@ -17,18 +17,29 @@ func TestPartition(t *testing.T) {
 		{ll.CreateLinkedList([]int{3, 5, 8, 5, 10, 2, 1}), 10},
 		{ll.CreateLinkedList([]int{1}), 1},
 		{ll.CreateLinkedList([]int{1, 2}), 3}, // edge
-		// {nil, 3}, // edge
+		{nil, 3}, // edge
 	}
 
 	for _, tt := range tests {
 		beforePivot := true
+		if tt.list == nil {
+			partition(tt.list, tt.pivot)
+			if tt.list != nil {
+				t.Errorf("parition(nil) does not return nil. got=%v\n",
+				tt.list)
+			}
+			continue
+		}
 		partition(tt.list, tt.pivot)
 		for node := tt.list.Head; node != nil; node = node.Next {
-			if beforePivot && node.Content > tt.pivot {
+			if node.Content >= tt.pivot {
+				beforePivot = false
+			}
+			if beforePivot && node.Content >= tt.pivot {
 				t.Errorf("partition error before pivot. got=%d, pivot=%d\n",
 				node.Content, tt.pivot)
 				break
-			} else if !beforePivot && node.Content <= tt.pivot {
+			} else if !beforePivot && node.Content < tt.pivot {
 				t.Errorf("partition error after pivot. got=%d, pivot=%d\n",
 				node.Content, tt.pivot)
 				break
